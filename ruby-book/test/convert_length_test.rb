1037b9a0eda170dd67e0d90ecc1367b8caf4df67
fimitaka shimizu <gilgala.aiglos@gmail.com>
2020-10-02 10:04:11 +0900

    first commit

---
+++ ruby-book/test/convert_length_test.rb
@@ -0,0 +1,9 @@
+# require 'minitest/autornu'
+# require './lib/convert_length'
+#
+#
+# class ConvertLengthTest < Minitest::Test
+#   def test_convert_length
+#     assert_equal 39.37, convert_length(1, 'm', 'in')
+#   end
+# end
---
+++ test.rb
@@ -0,0 +1,20 @@
+# number = [1,2,3,4]
+# sum = 0
+# number.each do |n|
+#   sum_value = n.even? ? n * 10 : n
+#   puts sum_value
+#   sum += sum_value
+#   end
+# #
+# def buy_burger(menu,option = {})
+#   drink = option[:drink]
+#   potato = option[:potato]
+#   puts menu
+#   puts drink
+#   puts potato
+# end
+#
+# buy_burger('cheese',drink: true, potato: true)
+
+kakaku = "入力したのは" + gets
+puts kakaku
